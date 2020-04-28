cd643d08edbade68681052688f159f3355a8600f
zhiyong.guo <zhiyong.guo@bioknow.net>
2020-04-28 18:04:38 +0800

    添加气球

--- app/src/main/java/com/example/game/balloon/BalloonStart.java
+++ app/src/main/java/com/example/game/balloon/BalloonStart.java
@@ -4,27 +4,42 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.LogRecord;
-
 
 public class BalloonStart extends Activity {
-    GameView gameView;
+    private GameView gameView;
+    private int winWidth;
+    private int winHeight;
+    private boolean right = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gameView = new GameView(this);
         setContentView(gameView);
+        //获取屏幕宽高
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        winWidth = metrics.widthPixels;
+        winHeight = metrics.heightPixels;
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                gameView.x++;
+                if (gameView.x>winWidth-200) right = false;
+                if (gameView.x<0) right = true;
+                if (right) gameView.x+=5;
+                else gameView.x-=5;
                 handler.sendEmptyMessage(0x123);
             }
         },0,15);
